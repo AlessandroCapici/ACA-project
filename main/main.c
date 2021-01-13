2e8caa599e06068313f7a4c06aa3ee9568188a14
AndreaPrati98 <andrea.prati01@universitadipavia.it>
2021-01-13 13:58:08 +0100

    prova risoluzione bug memorizzazione data point (non funziona)

--- main/main.c
+++ main/main.c
@@ -26,39 +26,34 @@ int main(int argc, char **argv) {
 	 - filename to write the coordinated of the centroid
 	 */
 
-/*
-	if (argc < 6) {
-		printf("Less no. of command line arguments\n\n");
-		return 0;
-	} else if (argc > 6) {
-		printf("Too many command line arguments\n\n");
-		return 0;
-	}
-
-	// Correct no. of command line arguments
-
-	const char *dataset_filename = argv[1];
-	const int K = atoi(argv[2]);
-	const int num_threads = atoi(argv[3]);
-	const char *data_points_output_filename = argv[4];
-*/
 
 	int N;	// Total no. of data_points in the file
 	point *data_points;	// array of data points
 	point *centroids;	// array of centroids of clusters
 	int num_iterations = 0;
 	
-	readDataset("../datasets/dataset_5.txt", &N, data_points);
+	printf("%d", &data_points);
+	readDataset("../datasets/dataset_5.txt", &N, &data_points);
+//	printf("%d", N);
 	
 	int i;
-	
 	for(i = 0; i < N; i++) {
+		printf("ciao	1\n");
+		printf("%d \n", i);
+		
 		int x, y, z;
+		printf("ciao	2\n");
+	
+		printf("%d", data_points[i].coordinates[0]);
 		x = data_points[i].coordinates[0];
 		y = data_points[i].coordinates[1];
 		z = data_points[i].coordinates[2];
-
+		
+		printf("ciao	3\n");
+	
 		printf("Point %d: x= %f y=%f z=%f", N, x, y, z);
+		printf("ciao	4\n");
+	
 	}
 	
 	free(data_points);
@@ -85,7 +80,6 @@ void readDataset(const char *filename, int *N_points, point *data_points) {
 	printf("The chars read are: %d \n", readChars);	
 	printf("No. of data points in the dataset: %d \n", *N_points);
 	
-
 	// Each data point is has so many coordinates as DIMENSION
 	data_points = malloc((*N_points) * sizeof(point));
 	
@@ -93,15 +87,18 @@ void readDataset(const char *filename, int *N_points, point *data_points) {
 //	}
 
 	int i;
-	printf("%d", *N_points);
+	
 	for (i = 0; i < (*N_points); i++) {
-		printf("pippo");
 		int x, y, z;
+		
 		fscanf(fptr, "%d %d %d", &x, &y, &z);
-		printf("%d %d %d", &x, &y, &z);
+		// Questa scan dovrebbe memorizzare dei float, non dei double
+		//fscanf(fptr, "%0.2f %0.2f %0.2f", &x, &y, &z);
+		//printf("%f %f %f \n", x, y, z);
 		data_points[i].coordinates[0] = x;
 		data_points[i].coordinates[1] = y;
 		data_points[i].coordinates[2] = z;
+		printf("%d %d %d \n", x, y, z);
 	}
 	
 	
