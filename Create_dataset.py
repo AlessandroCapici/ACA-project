f73b73b9b947952c836ff6b6d54adc6ef9faa1a6
AlessandroCapici <alessandro.capici01@universitadipavia.it>
2021-01-23 15:59:48 +0100

    aggiunto dataset da 20000000 di righe su drive perchè qui non si può

---
+++ Create_dataset.py
@@ -0,0 +1,12 @@
+import os
+import random
+
+file=open("big_dataset.txt","w")
+file.write("20000000\n")
+for i in range(1,20000000):
+    x=random.randint(-1000,1000)
+    y=random.randint(-1000,1000)
+    z=random.randint(-1000,1000)
+    file.write(str(x)+" "+str(y)+" "+str(z)+"\n")
+    print(i)
+file.close()
