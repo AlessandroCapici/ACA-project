ef296ec26833f0e38ea67d84ffe3f5cdeb2ad8fa
AlessandroCapici <alessandro.capici01@universitadipavia.it>
2021-02-17 11:13:42 +0100

    aggiunto python

--- Create_dataset.py
+++ Create_dataset.py
@@ -3,7 +3,7 @@ import random
 
 file=open("big_dataset.txt","w")
 file.write("20000000\n")
-for i in range(1,20000000):
+for i in range(1,2000000):
     x=random.randint(-1000,1000)
     y=random.randint(-1000,1000)
     z=random.randint(-1000,1000)
